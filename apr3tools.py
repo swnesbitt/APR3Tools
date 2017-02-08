74e7689fe939d764d9ed5316ef9c8790abce50dc
dopplerchase <randy.chase12@gmail.com>
2017-02-08 09:57:22 -0600

    Update apr3tools.py

--- apr3tools.py
+++ apr3tools.py
@@ -80,31 +80,35 @@ def apr3plot(filename,scan,band,fontsize=14,fontsize2=12,savefig=False,cmap = 's
         color_p = 'w'
         
     apr = apr3read(filename,scan,mask=mask)
-    time = apr['time_gate']
-    time_dates = apr['time_dates']
-    alt = apr['alt_gate']
-    plane = apr['alt_plane']
-    radar = apr[band]
-    surface = apr['surface']
-    pm = ax.pcolormesh(time,alt/1000.,radar,vmin=vmin,vmax=vmax,cmap=cmap)
-    ax.fill_between(time_dates,surface/1000.,-8.,color='k',edgecolor='w')
-    ax.plot(time_dates,plane/1000.,'-.',lw=3,color=color_p)
-    hfmt = dates.DateFormatter('%H:%M')
-    ax.xaxis.set_major_formatter(hfmt)
-    ax.set_ylim([0,max(plane/1000.)+0.1])
-    ax.set_ylabel('Altitude, [km]',fontsize=fontsize)
-    ax.set_xlabel('Time, [UTC]',fontsize=fontsize)
-    ax.set_title(time_dates[0],fontsize=fontsize+2)
-    ax.tick_params(axis='both',direction='in',labelsize=fontsize2,width=2,length=5,color='k')
-    cbar = plt.colorbar(pm,aspect = 10)
-    cbar.set_label(colorbarlabel,fontsize=fontsize)
-    cax = cbar.ax
-    cax.tick_params(labelsize=fontsize2)
     
-    if savefig:
-        print('Save file is: ' + str(time_dates[0])+'_'+band+'.png')
-        plt.savefig(str(time_dates[0])+'_'+band+'.png',dpi=300)
-    plt.show()
+    if (apr['W'].size==0 and band=='W') or (apr['W'].size==0 and band=='DFR_2') or (apr['W'].size==0 and band=='DFR_3'):
+        print()
+    else:
+        time = apr['time_gate']
+        time_dates = apr['time_dates']
+        alt = apr['alt_gate']
+        plane = apr['alt_plane']
+        radar = apr[band]
+        surface = apr['surface']
+        pm = ax.pcolormesh(time,alt/1000.,radar,vmin=vmin,vmax=vmax,cmap=cmap)
+        ax.fill_between(time_dates,surface/1000.,-8.,color='k',edgecolor='w')
+        ax.plot(time_dates,plane/1000.,'-.',lw=3,color=color_p)
+        hfmt = dates.DateFormatter('%H:%M')
+        ax.xaxis.set_major_formatter(hfmt)
+        ax.set_ylim([0,max(plane/1000.)+0.1])
+        ax.set_ylabel('Altitude, [km]',fontsize=fontsize)
+        ax.set_xlabel('Time, [UTC]',fontsize=fontsize)
+        ax.set_title(time_dates[0],fontsize=fontsize+2)
+        ax.tick_params(axis='both',direction='in',labelsize=fontsize2,width=2,length=5,color='k')
+        cbar = plt.colorbar(pm,aspect = 10)
+        cbar.set_label(colorbarlabel,fontsize=fontsize)
+        cax = cbar.ax
+        cax.tick_params(labelsize=fontsize2)
+
+        if savefig:
+            print('Save file is: ' + str(time_dates[0])+'_'+band+'.png')
+            plt.savefig(str(time_dates[0])+'_'+band+'.png',dpi=300)
+        plt.show()
     return
 
 def apr3flighttrack(filename):
@@ -157,7 +161,7 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     """
     
     apr = {}
-
+    flag = 0
     ##Radar varibles in hdf file found by hdf.datasets
     radar_freq = 'zhh14' #Ku
     radar_freq2 = 'zhh35' #Ka
@@ -167,6 +171,18 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     ##
 
     hdf = SD(filename, SDC.READ)
+    
+    listofkeys = hdf.datasets().keys()
+    if 'zvv95' in listofkeys:
+        radar3 = hdf.select(radar_freq3)
+        radar_n3 = radar3.get()
+        radar_n3 = radar_n3[:,scan,:]/100.
+    else:
+        radar_n3 = np.array([])
+        flag = 1
+        print('No W band')
+    
+    
     alt = hdf.select('alt3D')
     lat = hdf.select('lat')
     lon = hdf.select('lon')
@@ -176,7 +192,6 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     plane = hdf.select('alt_nav')
     radar = hdf.select(radar_freq)
     radar2 = hdf.select(radar_freq2)
-    radar3 = hdf.select(radar_freq3)
     radar4 = hdf.select(radar_freq4)
     vel = hdf.select(vel_str)
 
@@ -194,8 +209,7 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     radar_n = radar_n[:,scan,:]/100.
     radar_n2 = radar2.get()
     radar_n2 = radar_n2[:,scan,:]/100.
-    radar_n3 = radar3.get()
-    radar_n3 = radar_n3[:,scan,:]/100.
+
     radar_n4 = radar4.get()
     radar_n4 = radar_n4[:,scan,:]/100.
     vel_n = vel.get()
@@ -216,7 +230,10 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     ##Mask radar below surface
     radar_n[ind] = np.inf
     radar_n2[ind] = np.inf
-    radar_n3[ind] = np.inf
+    
+    if flag == 0:
+        radar_n3[ind] = np.inf
+        radar_n3 = np.ma.masked_invalid(radar_n3, copy=True)
     
     if mask:
         print('masking')
@@ -226,13 +243,13 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
         ind = np.where(radar_n2 < vmin)
         radar_n2[ind] = np.inf
         
-        ind = np.where(radar_n3 < vmin)
-        radar_n3[ind] = np.inf
+        if flag == 0:
+            ind = np.where(radar_n3 < vmin)
+            radar_n3[ind] = np.inf
         
 #    radar_n4[ind] = np.inf
     radar_n = np.ma.masked_invalid(radar_n, copy=True)
     radar_n2 = np.ma.masked_invalid(radar_n2, copy=True)
-    radar_n3 = np.ma.masked_invalid(radar_n3, copy=True)
     radar_n4 = np.ma.masked_invalid(radar_n4, copy=True)
 
     ##convert time to datetimes
@@ -249,8 +266,16 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     apr['Ka'] = radar_n2
     apr['W'] = radar_n3
     apr['DFR_1'] = radar_n - radar_n2 #Ku - Ka
-    apr['DFR_2'] = radar_n2 - radar_n3 #Ka - W
-    apr['DFR_3'] = radar_n - radar_n3 #Ku - W
+    
+    if flag == 0:
+        apr['DFR_2'] = radar_n2 - radar_n3 #Ka - W
+        apr['DFR_3'] = radar_n - radar_n3 #Ku - W
+        apr['info'] = 'The shape of these arrays are: Radar[Vertical gates,Time/DistanceForward]'
+    else:
+        apr['DFR_2'] = np.array([]) #Ka - W
+        apr['DFR_3'] = np.array([]) #Ku - W
+        apr['info'] = 'The shape of these arrays are: Radar[Vertical gates,Time/DistanceForward], Note No W band avail'
+        
     apr['ldr'] = radar_n4
     apr['vel'] = vel_n
     apr['lon'] = lon
@@ -260,6 +285,6 @@ def apr3read(filename,scan,mask=False,vmin=0,vmax=40):
     apr['surface'] = surfalt + newzero
     apr['time_gate']= time_new
     apr['time_dates'] = time_dates
-    apr['info'] = 'The shape of these arrays are: Radar[Vertical gates,Time/DistanceForward]'
+
     
     return apr
